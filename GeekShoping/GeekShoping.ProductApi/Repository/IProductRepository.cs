4b1a9a1a8293af4baf3c9eba8bc5fc1482ebdc1a
Luis Felipe Saldanha <89476616+Lfs1617@users.noreply.github.com>
2022-05-18 18:04:58 -0300

    Preenchendo a classe repository

--- GeekShoping/GeekShoping.ProductApi/Repository/IProductRepository.cs
+++ GeekShoping/GeekShoping.ProductApi/Repository/IProductRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace GeekShoping.ProductApi.Repository
+﻿using GeekShoping.ProductApi.Data.VO;
+
+namespace GeekShoping.ProductApi.Repository
 {
     public interface IProductRepository
     {
+        Task<IEnumerable<ProductVO>> FindAll();
+        Task<ProductVO> FindById(long id);
+        Task<ProductVO> Create(ProductVO vo);
+        Task<ProductVO> Update(ProductVO vo);
+        Task<bool> Delete(long id);
     }
 }
